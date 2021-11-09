198a02bb93c641ed9fad1f8f240a1e207f80360e
Ismail <ismtim095@gmail.com>
2021-11-09 11:23:28 +0300

    Сделал верстку

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,13 @@
+import { Container } from '@material-ui/core';
 import React from 'react';
-import Container from '@material-ui/core/Container';
-import Typography from '@material-ui/core/Typography';
-import { createMuiTheme } from '@material-ui/core/styles';
-import { ThemeProvider } from '@material-ui/styles';
-import { TodoItemsList } from './TodoItems';
-import { TodoItemsContextProvider } from './TodoItemsContext';
 import TodoItemForm from './TodoItemForm';
 
-const theme = createMuiTheme({
-    palette: {
-        primary: {
-            main: '#9012fe',
-        },
-        secondary: {
-            main: '#b2aabf',
-        },
-    },
-});
-
-function App() {
-    return (
-        <TodoItemsContextProvider>
-            <ThemeProvider theme={theme}>
-                <Content />
-            </ThemeProvider>
-        </TodoItemsContextProvider>
-    );
-}
-
-function Content() {
-    return (
-        <Container maxWidth="sm">
-            <header>
-                <Typography variant="h2" component="h1">
-                    Todo List
-                </Typography>
-            </header>
-            <main>
-                <TodoItemForm />
-                <TodoItemsList />
-            </main>
-        </Container>
-    );
-}
+const App: React.FC = () => {
+  return (
+    <Container maxWidth="sm">
+      <TodoItemForm />
+    </Container>
+  );
+};
 
 export default App;
--- src/TodoItemForm.tsx
+++ src/TodoItemForm.tsx
@@ -1,63 +1,28 @@
-import { useTodoItems } from './TodoItemsContext';
-import { useForm, Controller } from 'react-hook-form';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-import { makeStyles } from '@material-ui/core/styles';
+import { Button, makeStyles, TextField } from '@material-ui/core';
+import React from 'react';
 
-const useInputStyles = makeStyles(() => ({
-    root: {
-        marginBottom: 24,
-    },
+const useStyles = makeStyles((theme) => ({
+  root: {},
+  input: {
+    width: '100%',
+    marginBottom: 20,
+  },
 }));
 
-export default function TodoItemForm() {
-    const classes = useInputStyles();
-    const { dispatch } = useTodoItems();
-    const { control, handleSubmit, reset, watch } = useForm();
+const TodoItemForm: React.FC = () => {
 
-    return (
-        <form
-            onSubmit={handleSubmit((formData) => {
-                dispatch({ type: 'add', data: { todoItem: formData } });
-                reset({ title: '', details: '' });
-            })}
-        >
-            <Controller
-                name="title"
-                control={control}
-                defaultValue=""
-                rules={{ required: true }}
-                render={({ field }) => (
-                    <TextField
-                        {...field}
-                        label="TODO"
-                        fullWidth={true}
-                        className={classes.root}
-                    />
-                )}
-            />
-            <Controller
-                name="details"
-                control={control}
-                defaultValue=""
-                render={({ field }) => (
-                    <TextField
-                        {...field}
-                        label="Details"
-                        fullWidth={true}
-                        multiline={true}
-                        className={classes.root}
-                    />
-                )}
-            />
-            <Button
-                variant="contained"
-                color="primary"
-                type="submit"
-                disabled={!watch('title')}
-            >
-                Add
-            </Button>
-        </form>
-    );
-}
+  const classes = useStyles();
+
+  return (
+    <form className={classes.root} noValidate autoComplete="off">
+      <h1>Todo List</h1>
+      <TextField className={classes.input} id="standard-basic" label="Todo" />
+      <TextField className={classes.input} id="standard-basic" label="Details" />
+      <Button variant="contained" color="primary">
+        ADD
+      </Button>
+    </form>
+  );
+};
+
+export default TodoItemForm;
